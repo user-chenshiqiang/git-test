caf6157262e3d520566a6503e56103caa6c6d95d
user-chenshiqiang <134271229+user-chenshiqiang@users.noreply.github.com>
2023-05-30 16:31:51 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git master fix");
         System.out.println("hello git hot fix");
         System.out.println("新增远程库操作");
+        System.out.println("远程库新增");
     }
 }
